d0c2ad15e52471cf61130891eca018fe552fee42
billautomata <bill@evident.io>
2016-01-31 11:37:30 -0800

    more!

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # gate_level_sim
 building cpu components from gate level simulation
+
+reimplement logic level programming in javascript starting at terminals and gates (AND, NOT, OR, XOR)
+
+visualized in a network diagram
+
+At a system level, to preserve state the operations are split into a `operate()` and `write()` functions.  The main program will do a sweep of all the gates with the `operate` function.  Then do a sweep with the `write` function to set the state of the output terminals.
+
+```javascript
+var a = terminal()
+var b = terminal()
+var g = gate_and()
+g.init(a,b)
+
+a.set(0)
+b.set(0)
+g.operate()
+g.write()
+console.log(g.get())  // output, 0
+
+a.set(1)
+b.set(1)
+g.operate()
+g.write()
+console.log(g.get())  // 1, 1&&1 = 1
+
+a.set(0)
+g.operate()
+g.write()
+console.log(g.get())  // 0, 1&&0 = 0
+
+```
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function and_gate(){
 
-  var input_a = {}    // connected on init
+  // input terminal
+  var input_a = {}
   var input_b = {}
+
+  // result of the gate operation
+  var value = terminal()
+
+  // output terminal
   var output = terminal()
 
   function init(terminal_a, terminal_b){
@@ -10,16 +16,27 @@ function and_gate(){
 
   function operate(){
     if(input_a.v && input_b.v){
-      output.set(1)
+      value.set(1)
     } else {
-      output.set(0)
+      value.set(0)
     }
-    console.log('output', output.get())
+  }
+
+  function print(msg){
+    if(msg){
+      console.log(msg, 'value', value.get(), 'output', output.get())
+    } else {
+      console.log('value', value.get(), 'output', output.get())
+    }
   }
 
   function connect(a,b){
     input_a = a
     input_b = b
+  }
+
+  function write(){
+    output.set(value.get().v)
   }
 
   function get(){
@@ -30,27 +47,14 @@ function and_gate(){
     connect: connect,
     operate: operate,
     init: init,
-    get: get
+    get: get,
+    print: print,
+    write: write
   }
 
 }
 
-var PIN_0 = terminal()
-var PIN_1 = terminal()
-
-var gate = and_gate()
-gate.init(PIN_0, PIN_1)
-gate.operate()
-
-PIN_0.set(1)
-PIN_1.set(1)
-gate.operate()
-
-PIN_0.set(0)
-gate.operate()
-
-
-function terminal(){
+function terminal(){  // PIN
   var val = { v: 0 }
   function set(v){
     val.v = v
@@ -63,3 +67,24 @@ function terminal(){
     get: get
   }
 }
+
+var PIN_0 = terminal()
+var PIN_1 = terminal()
+
+var gate = and_gate()
+gate.init(PIN_0, PIN_1)
+gate.operate()
+gate.print('before write')
+gate.write()
+gate.print('after write')
+
+PIN_0.set(1)
+PIN_1.set(1)
+gate.operate()
+gate.write()
+gate.print()
+
+PIN_0.set(0)
+gate.operate()
+gate.write()
+gate.print()
