c5f4fb8b83e6c3a65ad6f5957c7383418650a811
Bill McKessy <bill@evident.io>
2016-10-15 14:37:56 -0700

    yes

--- circuits/auto_layout.js
+++ circuits/auto_layout.js
@@ -19,19 +19,23 @@ function auto_layout(){
 
   var gate_AND = gate.and()
   var gate_XOR = gate.xor()
+  var gate_OR = gate.or()
 
-  gate_AND.name = 'and'
-  gate_XOR.name = 'xor'
+  gate_AND.name = '1'
+  gate_XOR.name = '2'
+  gate_OR.name = '3'
 
   gates.push(gate_AND)
   gates.push(gate_XOR)
+  gates.push(gate_OR)
 
   // set the inputs of the gates
   gate_AND.init(A, B)
   gate_XOR.init(B, gate_AND.get())
+  gate_OR.init(gate_AND.get(), gate_XOR.get())
 
   // connect the outputs of the gates to the inputs of the terminals
-  O.set(gate_XOR.get())
+  O.set(gate_OR.get())
 
   outputs.push(O)
 
@@ -39,7 +43,7 @@ function auto_layout(){
   function tick(){
     console.log('hello from inside tick in auto_layout.js')
     gates.forEach(function(gate,idx){
-      console.log('checking gate', gate.name)
+      console.log('\n///////////////////////\nchecking gate', gate.name)
       // console.log(gate.wires())
 
       var wires = gate.wires()
@@ -50,7 +54,7 @@ function auto_layout(){
           if(wire_input === main_input.get()){
 
             console.log('\nfound a connection to an input terminal')
-            console.log(gate.name, 'input', gate_input_idx)
+            console.log('from', gate.name, 'input', gate_input_idx)
             console.log(main_input.get())
           }
         })
@@ -61,7 +65,7 @@ function auto_layout(){
         outputs.forEach(function(main_output, main_idx){
           if(wire_output === main_output.get()){
             console.log('\nfound a connection to an output terminal')
-            console.log(gate.name, 'output', gate_output_idx)
+            console.log('from', gate.name, 'output', gate_output_idx)
             console.log(main_output.get())
           }
         })
@@ -76,8 +80,8 @@ function auto_layout(){
             other_wires.in.forEach(function(other_wire_input, other_wire_input_idx){
               if(wire_output === other_wire_input){
                 console.log('\nfound a connection from one gate to another')
-                console.log(gate.name, 'output', gate_output_idx)
-                console.log(other_gate.name, 'input', other_wire_input_idx)
+                console.log('from', gate.name, 'output', gate_output_idx)
+                console.log('to', other_gate.name, 'input', other_wire_input_idx)
               }
             })
           }
